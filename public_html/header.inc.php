84060bfe0a30fe67c2c9f01df6ab6d2225a29bec
Kaldari <rkaldari@wikimedia.org>
2016-02-04 19:28:20 -0600

    switching to HTML5

--- public_html/header.inc.php
+++ public_html/header.inc.php
@@ -1,8 +1,8 @@
 <?php error_reporting(E_ERROR); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>Hot Articles List Generator</title>
 		<link rel="stylesheet" href="global.css" type="text/css" media="screen" />
 	</head>
